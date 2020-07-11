ea515cd2caa876efc4764520d5eccbedecee1b0a
Shyam-Chen <shyamchen1994@gmail.com>
2020-07-11 16:51:49 +0800

    58th Commit

--- client/mod-react/.rescriptsrc.js
+++ client/mod-react/.rescriptsrc.js
@@ -1,11 +1,21 @@
+const webpack = require('webpack');
+
 const { name } = require('./package');
 
 module.exports = {
   webpack: config => {
+    config.entry = config.entry.filter(
+      (e) => !e.includes('webpackHotDevClient')
+    );
+
     config.output.library = `${name}-[name]`;
     config.output.libraryTarget = 'umd';
     config.output.jsonpFunction = `webpackJsonp_${name}`;
     config.output.globalObject = 'window';
+
+    config.plugins = config.plugins.filter(
+      (p) => !(p instanceof webpack.HotModuleReplacementPlugin)
+    );
 
     return config;
   },
